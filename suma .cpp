9efce70b38949c31a1a95a641644177a303d3387
jeisira <salinasjeisira@gmail.com>
2021-04-22 20:45:47 -0500

    version1

---
+++ suma .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ void operacion(){
+ 	int v1;
+ 	int v2;
+ 	int suma;
+ 		cout<<"Primer valor"<<endl;
+	cin>>v1;
+	cout<<"segundo valor"<<endl;
+	cin>>v2;
+	suma =v1+v2;
+	cout<<"el resultado es"<<suma;
+ }
+	int main(){
+		operacion();
+	}
+
